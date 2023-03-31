830ff3fe94c0a444b7b96bfdd41b5f8f7e201571
ocamarli <108758911+ocamarli@users.noreply.github.com>
2023-03-30 20:35:19 -0600

    again

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     pass
 
 class DevelopmentConfig(Config):
-    DEBUG = True
+    DEBUG = False
     MONGO_DATABASE_URI = 'mongodb+srv://ocamar:xrfmX3Sr1YKv9S4k@vikkon.tdfkmfc.mongodb.net/demo?retryWrites=true&w=majority'
     MONGO_USERNAME = 'ocamar'
     MONGO_PASSWORD = 'xrfmX3Sr1YKv9S4k'
